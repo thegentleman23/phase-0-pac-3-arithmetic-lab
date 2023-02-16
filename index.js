e0d61641482fe9fbd6424b92cfb82d067e37f923
christophermichael <cmichaelcolonna@gmail.com>
2023-02-15 17:58:49 -0800

    complete lab

---
+++ index.js
@@ -0,0 +1,50 @@
+function add() {
+    // we'll fill this in shortly
+  }
+
+function subtract() {
+    // we'll fill this in shortly
+  }
+
+  function multiply() {
+    // we'll fill this in shortly
+  }
+
+  function divide() {
+    // we'll fill this in shortly
+  }
+
+  function add(a, b) {
+    return a + b;
+  }
+
+  function subtract(a, b) {
+    return a - b;
+  }
+
+  function multiply(a, b) {
+    return a * b;    
+  }
+  
+  function divide(a, b) {
+    return a / b;
+  }
+
+  function increment(n){
+    n++
+    return n
+    }
+
+    function decrement(n){
+        n--
+        return n
+    }
+
+    function makeInt(n) {
+        return parseInt(n, 10);
+    }
+
+    function preserveDecimal(n){
+        return parseFloat(n)
+    }
+    
